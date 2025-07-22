2aef06c5d694e7b4a6adb49268cd5d587483a4ec
Lal singh <lalsingh503511@gmail.com>
2025-07-22 21:46:03 +0530

    added blog model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title  = models.CharField(max_length=100)
+    content = models.TextField()
+    author= models.CharField(max_length=40)
+    # slug = models.SlugField(max_length=100, unique=True)
+    tags = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
